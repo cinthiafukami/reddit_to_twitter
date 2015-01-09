e9dfda4cdcc01c2339665f10d75eada08f3a1c0d
zgallup <zgallup@enova.com>
2015-01-09 04:20:17 -0600

    test

--- reddit_to_twitter.rb
+++ reddit_to_twitter.rb
@@ -3,6 +3,16 @@ require 'sinatra'
 require 'twitter'
 
 get '/' do
-  "Hello World"
-  # data = JSON.parse(request.body.read)
+  client = connect_to_twitter
+  data = JSON.parse(request.body.read)
+  client.update(data["title"])
+end
+
+def connect_to_twitter
+  Twitter::REST::Client.new do |config|
+    config.consumer_key        = "tKryxXxiORPQxuol2pIn0xYpm"
+    config.consumer_secret     = "hh4Q9JcwS6qf6Lr9fpi9wGq1JbVYLWDa6N7SVCTNhkdAegthqN"
+    config.access_token        = "269001389-8A9sIBhACPcksY83plUCuzy3hn3wcia5wMQJeXAc"
+    config.access_token_secret = "mtMyTxVUh9MdAO76sbVMU3u7cUJKumCnm4WWhotP0y9K5"
+  end
 end
